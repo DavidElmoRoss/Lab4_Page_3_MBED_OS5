0f2b932a2cb313505639b4a3d74df2f002341646
David Elmo Ross <110311948+DavidElmoRoss@users.noreply.github.com>
2022-10-22 12:27:43 -0400

    Update main.cpp
    updated comments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*
 Title:      Program to display POT values on PUTTY screen and colours on leds
-Author:     Dave Ross, Student ID, MCO455 Section __
+Author:     Your Name Here, Student ID, MCO455 Section __
 Date:       Today's date
 Description:    This program  will display  values from
                 the GROVE POT on the PUTTY screen and will turn on
@@ -10,8 +10,8 @@ Description:    This program  will display  values from
 */
 
 #include "mbed.h"
-void clear(void);                     // clears the PC screen
-void position(int x, int y);            // positions screen cursor at (x,y)
+void clear(void);                           // clears the PC screen
+void position(int x, int y);                // positions screen cursor at (x,y)
 
 // Hardware pin Assignments
 AnalogIn pot_input(A1);                     // POT connected to pin A1 on header
@@ -23,46 +23,46 @@ DigitalOut blue_led(LED3);
 int main()
 {
     float value;
-    red_led=green_led=blue_led=1;       // shut off all leds
-    clear();                            // clear the screen
-    position(11,32);                    // position title 
+    red_led=green_led=blue_led=1;           // shut off all leds
+    clear();                                // clear the screen
+    position(11,32);                        // position title 
     printf("\e[1m");
     printf("Temperature");
     position(13,33);
-    printf("\e[0m");                // shut off all colours
-    printf("          ");           // clear cool normal hot messgae
+    printf("\e[0m");                       // shut off all colours
+    printf("          ");                  // clear cool normal hot messgae
     for(;;)
     {
         value=pot_input.read();            // get FLOAT value for POT input
         value=value*100;
-        position(12,34);                // ouput both values on screen
-        printf("\e[0m");                // shut off all colours
+        position(12,34);                   // ouput both values on screen
+        printf("\e[0m");                   // shut off all colours
         printf("\e[1;37m");                // make bold white letters
         printf("%6.2f",value);
         if(value <33.0)
         {
-            red_led=green_led=1;        // turn off red and green leds
-            blue_led=0;                // turn on Green Led
+            red_led=green_led=1;           // turn off red and green leds
+            blue_led=0;                    // turn on Green Led
             position(13,33);
-            printf("\e[1;37;44m");           // set text colour to blue
+            printf("\e[1;37;44m");         // set text colour to blue
             printf("   COLD   ");
             
         }
         else
         if(value < 66.0)
         {
-            red_led=blue_led=1;         // turn off red and blue leds
-            green_led=0;                // turn on green led
+            red_led=blue_led=1;            // turn off red and blue leds
+            green_led=0;                   // turn on green led
             position(13,33);
-            printf("\e[1;30;42m");           // set text colour to blue
+            printf("\e[1;30;42m");         // set text colour to blue
             printf("  NORMAL  ");            
         }
         else
         {
-            green_led=blue_led=1;       // turn off green and blue
+            green_led=blue_led=1;          // turn off green and blue
             red_led=0;
             position(13,33);
-            printf("\e[1;37;41m");           // set text colour to blue
+            printf("\e[1;37;41m");         // set text colour to blue
             printf("  DANGER  ");        
         }
         wait(0.1);
@@ -71,11 +71,11 @@ int main()
 
 void clear(void)
 {
-       printf("\e[2J");                 // \e[2J clears the screen
+       printf("\e[2J");                   // \e[2J clears the screen
        fflush(stdout);
 }
 void position(int x, int y)
 {
-      printf("\e[%d;%dH",x,y);          // position cursor at (x,y)
+      printf("\e[%d;%dH",x,y);            // position cursor at (x,y)
       fflush(stdout);
 }
